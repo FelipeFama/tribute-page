3f304a876461429074b612ac60b975eac2a103e9
FelipeFama <lipehfama@gmail.com>
2024-01-18 20:41:21 -0300

    feat: add responsive style

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+function toggleMenu() {
+  let menuToggle = document.querySelector(".toggle");
+  let navigation = document.querySelector(".navigation");
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
+}
+
 window.onscroll = () => {
   if (window.scrollY > 60) {
     document.querySelector("#scroll-top").classList.add("active");
