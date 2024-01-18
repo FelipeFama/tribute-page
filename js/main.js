99473d5d1ebc60063b5e7635500711e8cbe1332d
FelipeFama <lipehfama@gmail.com>
2024-01-18 18:49:19 -0300

    feat: add scroll top

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.onscroll = () => {
+  if (window.scrollY > 60) {
+    document.querySelector("#scroll-top").classList.add("active");
+  } else {
+    document.querySelector("#scroll-top").classList.remove("active");
+  }
+};
